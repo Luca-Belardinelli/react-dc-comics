d0a334a7544ae078be95855760118e6f3b9e8021
Luca Belardinelli <belardinelliluca@gmail.com>
2025-02-12 16:29:43 +0100

    [divisione pagina jsx]

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// Header.jsx
+export default function Header() {
+    return (
+        <header>
+            <figure>
+                <img src="" alt="" />
+            </figure>
+            <nav>
+                <h1>CIAO</h1>
+            </nav>
+        </header>
+    );
+}
