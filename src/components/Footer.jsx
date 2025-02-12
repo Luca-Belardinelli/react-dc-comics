daf034181d78a8c61b81c273c4083499fb155b70
Luca Belardinelli <belardinelliluca@gmail.com>
2025-02-12 22:13:23 +0100

    [footer]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,65 @@
 export default function Main() {
     return (
         <footer>
-            <div className="container"></div>
+            <div className="container">
+                <div className="help">
+                    <div className="lowlinks">
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <h4>SHOP</h4>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div className="lowlinks">
+                        <h4>DC</h4>
+                        <ul>
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                    <div className="lowlinks">
+                        <h4>SITES</h4>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="img2"></div>
+            </div>
+            <div className="end">
+                <button>SIGN-UP NOW!</button>
+                <div className="icon">
+                    <h4>FOLLOW US</h4>
+                    <img src="../img/footer-facebook.png" alt="" />
+                    <img src="../img/footer-twitter.png" alt="" />
+                    <img src="../img/footer-youtube.png" alt="" />
+                    <img src="../img/footer-pinterest.png" alt="" />
+                    <img src="../img/footer-periscope.png" alt="" />
+                </div>
+            </div>
         </footer>
     );
 }
