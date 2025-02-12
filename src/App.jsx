ba7ce949df49fcfe04d2e8aa77b56196b3853eb5
Luca Belardinelli <belardinelliluca@gmail.com>
2025-02-12 17:20:21 +0100

    [header hover a]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header.jsx";
-import Main from "./components/Main.jsx";
-import Footer from "./components/Footer.jsx";
+// import Main from "./components/Main.jsx";
+// import Footer from "./components/Footer.jsx";
 
 // import './App.css'
 
@@ -8,8 +8,6 @@ function App() {
   return (
     <>
       <Header />
-      <Main />
-      <Footer />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,21 @@ export default function Header() {
     return (
         <header>
             <figure>
-                <img src="" alt="" />
+                <img src="/dc-logo.png" alt="Logo" />
             </figure>
             <nav>
-                <h1>CIAO</h1>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
             </nav>
         </header>
     );
